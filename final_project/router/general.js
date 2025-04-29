52e8fae2bc6cfd5ee2225f80685ec911610e85ff
basith <basith.anwarkcr.com>
2025-04-29 12:01:45 -0400

    authentification

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,14 +17,24 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn=req.params.isbn;
+    let book=books[isbn];
+    if(book){
+        res.send(JSON.stringify(book));
+    }else{
+        return res.status(403).json({message: "ISBN invalid"});
+    }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author=req.params.author;
+    let bookbyAuth=Object.values(books).filter(book=>book.author===author);
+    if(author){
+        res.send(JSON.stringify(bookbyAuth));
+    }else{
+        return res.status(403).json({message: "ISBN invalid"});
+    }
 });
 
 // Get all books based on title
